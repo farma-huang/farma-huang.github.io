0869b4c2c06b7902e86a424880a432704a9dc1ba
Farma Huang <farmahuang@gmail.com>
2023-01-14 13:55:48 +0800

    change 11ty output to docs

--- .eleventy.js
+++ .eleventy.js
@@ -267,7 +267,7 @@ module.exports = function (eleventyConfig) {
       includes: "_includes",
       data: "_data",
       // Warning hardcoded throughout repo. Find and replace is your friend :)
-      output: "_site",
+      output: "docs",
     },
   };
 };
